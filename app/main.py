93f2f8526ac64310d1ab9e6dfd8cccba597a46b3
Gajraj Chakrawarti <156901405+gajrajchakrawarti12@users.noreply.github.com>
2025-01-11 12:43:58 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ def main():
 
         elif user_input.split(" ")[0] == "echo":
             print(user_input.split(' ', 1)[1])
-            
+
         elif user_input.split(" ")[0] == "type":
-            if user_input.split(" ")[1] is ["echo", "exit", "type"]:
+            if user_input.split(" ")[1] in ["echo", "exit", "type"]:
                 print(user_input.split(" ")[1] + " is a shell builtin")
             else:
                 print(user_input.split(" ")[1] + " not found")
