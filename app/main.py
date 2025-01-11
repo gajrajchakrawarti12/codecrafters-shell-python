3aac994a260df608d779d9f1f00c3d16075aa2b5
Gajraj Chakrawarti <156901405+gajrajchakrawarti12@users.noreply.github.com>
2025-01-11 13:17:34 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -33,11 +33,11 @@ def main():
             else:
                 sys.stdout.write(cmd + " not found\n")
 
-        elif os.path.isfile(user_input.split(" ")[1]):
-                os.system(user_input)
-
         else:
-            print(f"{user_input}: command not found")
+            if os.path.isfile(user_input.split(" ")[0]):
+                os.system(user_input)
+            else:
+                print(f"{user_input}: command not found")
             
     
 
