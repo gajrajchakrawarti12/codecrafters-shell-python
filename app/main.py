143303b5d4a2a75580a046829955ef4f200bd6d8
Gajraj Chakrawarti <156901405+gajrajchakrawarti12@users.noreply.github.com>
2025-01-12 15:18:34 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -69,6 +69,12 @@ def main():
                                 result = subprocess.run(cmd_part, stdout=f, stderr=subprocess.PIPE, text=True)
                                 if result.stderr:
                                     print(f"Error: {result.stderr.strip()}")
+                        elif "2>" in parts:
+                            cmd_part = parts[:parts.index('2>')]
+                            output_file = parts[parts.index('2>') + 1]
+                            with open(output_file, 'w') as f:
+                                result = subprocess.run(cmd_part, stdout=f, stderr=subprocess.PIPE, text=True)
+                                f.write(str(result.stderr))
                         else:
                             sys.stdout.write(" ".join(parts[1:]) + "\n")
                     except Exception as e:
