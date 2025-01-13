961eea526760347a58139ea6ed15193cadee85a9
Gajraj Chakrawarti <156901405+gajrajchakrawarti12@users.noreply.github.com>
2025-01-13 20:27:11 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ def execute_command(cmd_parts, f1=None, f2=None):
         if result.stderr:
             sys.stdout.write(result.stderr)
         elif result.stdout:
-            if 'echo' not in cmd_parts:
+            if 'echo' != cmd_parts[0]:
                 sys.stdout.write(result.stdout)
     except FileNotFoundError:
         sys.stderr.write(f"{cmd_parts[0]}: command not found\n")
